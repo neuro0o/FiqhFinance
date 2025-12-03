bcaf2c48567e8c3091d5903eaf24243e8e306d60
neuro0o <o.oneuro.o@gmail.com>
2025-12-03 13:40:42 +0800

    initial module layout template

--- resources/views/module 4/note.blade.php
+++ resources/views/module 4/note.blade.php
@@ -1,1 +1,44 @@
-Module 4 Note
+@extends('layouts.default')
+
+<!-- TITLE -->
+@section('title', 'Module 4 Note')
+
+<!-- PAGE SPECIFIC CSS -->
+@section('page-css')
+    <link rel="stylesheet" href="{{ asset('css/utils/sidebar.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/moduleNote/moduleNote4.css') }}">
+@endsection
+
+<!-- HEADER SECTION -->
+@section('header')
+    
+@endsection
+
+<!-- CONTENT SECTION -->
+@section('content')
+    @include('layouts.partials.sidebar')
+    <div class="note-content">
+        <h1>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-320q102-92 131-129.5t29-74.5q0-36-26-62t-62-26q-21 0-40.5 8.5T480-580q-12-15-31-23.5t-41-8.5q-36 0-62 26t-26 62q0 19 5 35t22 37.5q17 21.5 48.5 52.5t84.5 79Zm0 240q-139-35-229.5-159.5T160-516v-244l320-120 320 120v244q0 152-90.5 276.5T480-80Zm0-84q104-33 172-132t68-220v-189l-240-90-240 90v189q0 121 68 220t172 132Zm0-316Z"/></svg>
+            Module 4: Takaful
+        </h1>
+
+        <div class="minigameButton">
+            <a href="{{ route('module4.minigame') }}">
+                <button class="btn btn-primary btn-minigame">Test Your Knowledge</button>
+            </a>
+        </div>
+    </div>
+@endsection
+
+
+<!-- FOOTER SECTION -->
+@section('footer')
+    
+@endsection
+
+<!-- PAGE SPECIFIC JS -->
+@section('page-js')
+    <!-- FIXME: Fix Sidebar Collapse Behavior -->
+    <script src="{{ asset('js/sidebar.js') }}"></script>
+@endsection
