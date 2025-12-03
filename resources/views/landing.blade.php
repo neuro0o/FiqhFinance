602c3abb6e92c57d781189c207e53ed993d01315
neuro0o <o.oneuro.o@gmail.com>
2025-12-03 10:45:21 +0800

    Landing page done

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,4 +1,81 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    test
-</html>
+@extends('layouts.default')
+
+<!-- TITLE -->
+@section('title', 'FiqhFinance Landing Page')
+
+<!-- PAGE SPECIFIC CSS -->
+@section('page-css')
+    <link rel="stylesheet" href="{{ asset('css/auth/landing.css') }}">
+@endsection
+
+<!-- PAGE SPECIFIC JS -->
+@section('page-js')
+
+@endsection
+
+<!-- HEADER SECTION -->
+@section('header')
+    
+@endsection
+
+<!-- CONTENT SECTION -->
+@section('content')    
+    <section class="hero">
+        <div class="hero-content">
+            <img src="{{ asset('images/FiqhFinance_Logo.svg') }}" alt="FiqhFinance Logo"> 
+            <h1>Learn Islamic Finance, the fun way!</h1> 
+            <p style="font-style: italic;">From basic Islamic Principles, all the way to smart Halal investment</p>
+            <div class="button-groups">
+                <a href="{{ route('register') }}" class="btn btn-secondary">Register</a>
+                <a href="{{ route('login') }}" class="btn btn-primary">Login</a>
+            </div>
+        </div>
+    </section>
+
+    <!-- FiqhFinance Features Section -->
+    <section class="feature">
+        <h2>Why FiqhFinance?</h2>
+        <div class="feature-groups">
+            @php
+            $features = [
+                ['icon'=>'Gamified Learning.svg','title'=>'GAMIFIED LEARNING','desc'=>'Learn through digital notes, mini-games, quizzes, and challenges!'],
+                ['icon'=>'Earn Badges.svg','title'=>'EARN BADGES','desc'=>'Complete learning modules and unlock fun achievement badges!'],
+                ['icon'=>'Shariah Based.svg','title'=>'SHARIAH BASED','desc'=>'All content follows Islamic Financial Principles!'],
+            ];
+            @endphp
+
+            @foreach($features as $feature)
+            <div class="feature-card">
+                <img src="{{ asset('images/icons/'.$feature['icon']) }}" alt="{{ $feature['title'] }}">
+                <div class="feature-text">
+                    <h5><strong>{{ $feature['title'] }}</strong></h5>
+                    <p>{{ $feature['desc'] }}</p>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </section>
+
+    <!-- Call-To-Action Section -->
+    <section class="cta">
+        <div class="cta-content">
+            <h2>Start Your Halal Finance Journey Today!</h2>
+            <p>Join and level up your Islamic Finance Literacy!.</p>
+            <div class="button-groups">
+                <a href="{{ route('register') }}" class="btn btn-secondary">Register Now</a>
+                <a href="{{ route('login') }}" class="btn btn-primary">Login</a>
+            </div>
+        </div>
+    </section>
+
+@endsection
+
+<!-- FOOTER SECTION -->
+@section('footer')
+    
+@endsection
+
+<!-- PAGE SPECIFIC JS -->
+@section('page-js')
+    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,53 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RegistrationController;
+use App\Http\Controllers\UserDashboardController;
+use App\Http\Controllers\AccountSettingsController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
+// LANDING
 Route::get('/', function () {
     return view('landing');
+    // return view('user/dashboard');
 });
+
+/*----------------- AUTH ROUTES -------------------*/
+
+// REGISTER
+Route::get('register', [RegistrationController::class, 'showForm'])
+    ->name('register');
+Route::post('register', [RegistrationController::class, 'processForm'])
+    ->name('register.process');
+
+// LOGIN
+Route::get('login', [AuthController::class, 'showLoginForm'])
+    ->name('login');
+Route::post('login', [AuthController::class, 'login'])
+    ->name('login.process');
+
+// Logout
+Route::post('/logout', function (Request $request) {
+    Auth::logout();
+    $request->session()->invalidate();
+    $request->session()->regenerateToken();
+    return redirect('/'); // landing page
+})->name('logout');
+
+// Account Settings
+Route::middleware(['auth'])->prefix('account')->group(function () {
+    Route::get('/settings', [AccountSettingsController::class, 'index'])->name('account.settings');
+    Route::put('/update', [AccountSettingsController::class, 'updateProfile'])->name('account.update');
+    Route::put('/update-password', [AccountSettingsController::class, 'updatePassword'])->name('account.update.password');
+    Route::delete('/account/delete', [AccountSettingsController::class, 'deleteAccount'])->name('account.delete');
+
+    // Route for AJAX modal
+    Route::get('/settings/modal', [AccountSettingsController::class, 'modal'])->name('account.settings.modal');
+});
+
+// USER HOME @ DASHBOARD
+Route::get('/dashboard', function () {
+    return view('user.dashboard');
+})->middleware('auth')->name('user.dashboard');
